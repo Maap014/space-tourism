9eec4e34429bf87c0902f4f00db16f8c876a0c9d
Maap014 <mayakiaugustine0@gmail.com>
2024-04-14 14:40:44 +0100

    UI implementation of crew page TandD view

--- src/Pages/Crew.js
+++ src/Pages/Crew.js
@@ -10,40 +10,44 @@ const Crew = () => {
     <div className="wrapper">
       <div className="crew-bg">
         <Navbar />
-
-        <div>
+        <div className="crew-container">
           <p className="page-heading">
             <span className="page-heading-id">02</span>
             MEET YOUR CREW
           </p>
-          <Carousel slide={slide} />
-          <div className="demaction-line" style={{ margin: "0px" }}></div>
-          <div className="crew-info">
-            <span className="indicators">
-              {CrewData?.crew?.map((_, idx) => {
-                return (
-                  <button
-                    key={idx}
-                    onClick={() => setSlide(idx)}
-                    className={
-                      slide === idx
-                        ? "indicator"
-                        : "indicator indicator-inactive"
-                    }
-                  ></button>
-                );
-              })}
-            </span>
-            <div>
-              <h3 className="crew-role">
-                {CrewData?.crew[slide].role.toLocaleUpperCase()}
-              </h3>
-              <h1 className="crew-name">
-                {CrewData?.crew[slide].name.toLocaleUpperCase()}
-              </h1>
-              <>
-                <p className="crew-bio">{CrewData?.crew[slide].bio}</p>
-              </>
+          <div className="crew-content">
+            <Carousel slide={slide} />
+            <div
+              className="crew-demaction-line"
+              style={{ margin: "0px" }}
+            ></div>
+            <div className="crew-info">
+              <span className="indicators">
+                {CrewData?.crew?.map((_, idx) => {
+                  return (
+                    <button
+                      key={idx}
+                      onClick={() => setSlide(idx)}
+                      className={
+                        slide === idx
+                          ? "indicator"
+                          : "indicator indicator-inactive"
+                      }
+                    ></button>
+                  );
+                })}
+              </span>
+              <div>
+                <h3 className="crew-role">
+                  {CrewData?.crew[slide].role.toLocaleUpperCase()}
+                </h3>
+                <h1 className="crew-name">
+                  {CrewData?.crew[slide].name.toLocaleUpperCase()}
+                </h1>
+                <>
+                  <p className="crew-bio">{CrewData?.crew[slide].bio}</p>
+                </>
+              </div>
             </div>
           </div>
         </div>
