d0439ab6e02a56dba9f3f8566e643787c67bfee6
Maap014 <mayakiaugustine0@gmail.com>
2024-04-14 10:59:48 +0100

    UI implementation of destination page TandD view, added roate animination

--- src/components/destinations/DestinationData.jsx
+++ src/components/destinations/DestinationData.jsx
@@ -2,27 +2,31 @@ import React from "react";
 
 const DestinationData = ({ planetData }) => {
   return (
-    <>
+    <div>
       <div>
         <h1 className="active-dest-name">
           {planetData.name.toLocaleUpperCase()}
         </h1>
       </div>
       <div>
-        <p style={{ lineHeight: "25px" }}>{planetData.description}</p>
+        <p className="dest-content">{planetData.description}</p>
       </div>
       <div className="demaction-line"></div>
-      <div>
-        <div className="distance-time-container">
-          <span className="travel-distance-time">Avg. Distance</span>
-          <p className="travel-distance-time-value">{planetData.distance}</p>
+      <div className="distance-time-container">
+        <div>
+          <span className="travel-distance-time">AVG. DISTANCE</span>
+          <p className="travel-distance-time-value">
+            {planetData.distance.toLocaleUpperCase()}
+          </p>
         </div>
-        <div className="distance-time-container">
-          <span className="travel-distance-time">Est. Travle Time</span>
-          <p className="travel-distance-time-value">{planetData.travel}</p>
+        <div>
+          <span className="travel-distance-time">EST. TRAVLE TIME</span>
+          <p className="travel-distance-time-value">
+            {planetData.travel.toLocaleUpperCase()}
+          </p>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
