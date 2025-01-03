01b92ab94dd91a6a1f4507ca4d8fb47536a5ee5a
Augustine <mayakiaugustine0@gmail.com>
2025-01-03 23:44:39 +0100

    implemented loading state

--- src/Pages/Crew.js
+++ src/Pages/Crew.js
@@ -2,54 +2,71 @@ import React, { useState } from "react";
 import Navbar from "../components/navbar/Navbar";
 import Carousel from "../components/Carousel/Carousel";
 import { useFetchData } from "./useFetchData";
+import { Spinner } from "../components/Loader/spinner";
 
 const Crew = () => {
   const [slide, setSlide] = useState(0);
-  const { planetData: CrewData } = useFetchData("/data.json");
+  const { planetData: CrewData, isLoading } = useFetchData("/data.json");
   return (
     <div className="wrapper">
       <div className="crew-bg">
         <Navbar />
         <div className="crew-container">
-          <p className="page-heading">
-            <span className="page-heading-id">02</span>
-            MEET YOUR CREW
-          </p>
-          <div className="crew-content">
-            <Carousel slide={slide} />
+          {!isLoading ? (
             <div
-              className="crew-demaction-line"
-              style={{ margin: "0px" }}
-            ></div>
-            <div className="crew-info">
-              <span className="indicators">
-                {CrewData?.crew?.map((_, idx) => {
-                  return (
-                    <button
-                      key={idx}
-                      onClick={() => setSlide(idx)}
-                      className={
-                        slide === idx
-                          ? "indicator"
-                          : "indicator indicator-inactive"
-                      }
-                    ></button>
-                  );
-                })}
-              </span>
-              <div>
-                <h3 className="crew-role">
-                  {CrewData?.crew[slide].role.toLocaleUpperCase()}
-                </h3>
-                <h1 className="crew-name">
-                  {CrewData?.crew[slide].name.toLocaleUpperCase()}
-                </h1>
-                <>
-                  <p className="crew-bio">{CrewData?.crew[slide].bio}</p>
-                </>
-              </div>
+              style={{
+                width: "100%",
+                height: 500,
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Spinner />
             </div>
-          </div>
+          ) : (
+            <>
+              <p className="page-heading">
+                <span className="page-heading-id">02</span>
+                MEET YOUR CREW
+              </p>
+              <div className="crew-content">
+                <Carousel slide={slide} />
+                <div
+                  className="crew-demaction-line"
+                  style={{ margin: "0px" }}
+                ></div>
+                <div className="crew-info">
+                  <span className="indicators">
+                    {CrewData?.crew?.map((_, idx) => {
+                      return (
+                        <button
+                          key={idx}
+                          onClick={() => setSlide(idx)}
+                          className={
+                            slide === idx
+                              ? "indicator"
+                              : "indicator indicator-inactive"
+                          }
+                        ></button>
+                      );
+                    })}
+                  </span>
+                  <div>
+                    <h3 className="crew-role">
+                      {CrewData?.crew[slide].role.toLocaleUpperCase()}
+                    </h3>
+                    <h1 className="crew-name">
+                      {CrewData?.crew[slide].name.toLocaleUpperCase()}
+                    </h1>
+                    <>
+                      <p className="crew-bio">{CrewData?.crew[slide].bio}</p>
+                    </>
+                  </div>
+                </div>
+              </div>
+            </>
+          )}
         </div>
       </div>
     </div>
--- src/Pages/useFetchData.js
+++ src/Pages/useFetchData.js
@@ -2,15 +2,21 @@ import { useState, useEffect } from "react";
 
 export const useFetchData = (url) => {
   const [planetData, setPlanetData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    setIsLoading(true);
     fetch(url)
       .then((res) => res.json())
       .then((data) => {
+        setIsLoading(false);
         setPlanetData(data);
       })
-      .catch((err) => console.error("Cant fetch:", err));
+      .catch((err) => {
+        setIsLoading(false);
+        console.error("Cant fetch:", err);
+      });
   }, [url]);
 
-  return { planetData };
+  return { planetData, isLoading };
 };
